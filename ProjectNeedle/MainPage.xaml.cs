9aeb25767ae67afca15e1edd30ad8772261a5aee
Cristofer Binimelis <cristoferbinimelis@gmail.com>
2024-02-21 13:10:53 -0300

    Nuget Packages implementation

--- ProjectNeedle/MainPage.xaml.cs
+++ ProjectNeedle/MainPage.xaml.cs
@@ -1,10 +1,29 @@
-﻿namespace ProjectNeedle
+﻿using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Storage;
+using CommunityToolkit.Mvvm.Input;
+using System.Threading.Tasks;
+
+namespace ProjectNeedle
 {
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        [RelayCommand]
+        async Task PickFolder(, CancellationToken cancellationToken)
+        {
+            var result = await FolderPicker.Default.PickAsync(cancellationToken);
+            if (result.IsSuccessful)
+            {
+                await Toast.Make($"The folder was picked: Name - {result.Folder.Name}, Path - {result.Folder.Path}").Show(cancellationToken);
+            }
+            else
+            {
+                await Toast.Make($"The folder was not picked with error: {result.Exception.Message}").Show(cancellationToken);
+            }
         }
     }
 
